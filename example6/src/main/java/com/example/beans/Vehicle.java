cda83e4dd3925f50026344416fa30dd332a5c932
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-05-21 16:49:18 +0300

    @PostConstruct & @PreDestroy Annotations

---
+++ example6/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,34 @@
+package com.example.beans;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class Vehicle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello() {
+        System.out.println("Print Hello World from Component Vehicle Bean");
+    }
+
+    @PostConstruct
+    public void initialize() {
+        this.name = "Honda";
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Destroying Vehicle Bean ");
+    }
+}
